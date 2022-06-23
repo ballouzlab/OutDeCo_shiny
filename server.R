36a8f5aa5fe834debd126a562ab9ef5ec7f1aa9f
Jasmin Y <z5310015@ad.unsw.edu.au>
2022-06-23 18:15:43 +1000

    SHIN14 - description for DA and Cluster

--- server.R
+++ server.R
@@ -1,5 +1,13 @@
 server <- function(input, output) {
+  theme = bs_theme(version = 4, bootswatch = "minty")
   output$distPlot <- renderPlot({
     hist(rnorm(input$obs), col = 'darkgray', border = 'white')
   })
+  output$selected_var <- renderText({ 
+    "You have selected this"
+  })
+  output$table <- DT::renderDT({
+    iris
+  })
+  output$text <-  renderText({ output$txt })
 }
--- ui.R
+++ ui.R
@@ -1,54 +1,100 @@
 library(shinyWidgets)
 library(shinythemes)
+library(bslib)
+library(DT)
+
+
 ui <- fluidPage(
-  titlePanel(title=div(img(src="ODClogo.png", height = 50), "OutDeCo")),
-  shinythemes::themeSelector(),
-  navbarPage("",
-             
+ 
+  titlePanel(title=div(img(src="ODClogo.png", height = 80), "OutDeCo")),
+  theme = bs_theme(version = 5, bootswatch = "sandstone"),
+  
+  navbarPage("", collapsible = TRUE,
+          
              tabPanel(
                title="Home",
+               icon = icon("home"),
 
                navlistPanel(
-                 id = NULL, selected = NULL, well = FALSE, fluid = FALSE, widths = c(3, 8) ,
-                 
-                 tabPanel(
-                   title="About",
+                 id = "Header", selected = NULL, well = FALSE, fluid = FALSE, widths = c(3, 8) ,
+             
+                 tabPanel(title="About",
                    h3("What is OutDeCo?"),
-                   h4(""),
-                   h5("Header 5"),  
+                   h6("Outlier detection through co-expression. Using the tools on this website you can:"),
+                   p("- Run a differential expression (DE) analysis "),
+                   p("- Assess your DE results using gene co-expression properties"),
+                   p("- Report a functional outlier assessment"),
+                   p("- Run a network connectivity analysis of DE results within a gene co-expression network"),
                  ),
                  
-                 tabPanel(
-                   title="Differential Analysis",
-                   "information about DE",
-                 ),
-                 
-                 
-                 tabPanel(
-                   title="Cluster Genes",
-                   h2("Cluster Genes"),
-                   h3("Plot Types"),
-                   h4("Heatmap"),  
-                   h5("text"),
-                   h4("Network"),  
-                   h4("Binarized heatmap"),  
+                 tabPanel(title="Differential Analysis",
+                   h3("Differential Analysis Methods"),
+                   h5("Methods:"),
+                   h6("wilcox"),
+                   p("Compares means of two groups to analyse count data and test for differential expression."),
+                   h6("DESeq"),
+                   p("Uses geometric normalisation to analyse count data and test for differential expression."),
+                   h6("edgeR"),
+                   p("Uses weighted mean of log ratio to analyse count data and test for differential expression."),
+                   br(),
+                   em("Note: This tool is under construction")
                    
+                  ),
+                 
+                 
+                 tabPanel(title="Cluster Genes",
+                   h3("Cluster Genes"),
+                   p('Creates modules which are clusters of genes that are hightly co-expressed'),
+                   h5("Plot Types"),
+                   h6("Heatmap"),  
+                   p("up-regulated or down-regulated network plot"),
+                   h6("Network"),  
+                   p("up regulated or down-regulated network plot where nodes are genes and the weight of the edges corresponds to the co-expression levels between its endpoints"),
+                   h6("Binarized heatmap"),  
+                   p("up-regulated or down-regulated binary co-expression sub-network"),
+                   br(),
+                   em("Note: This tool is under construction")
                  ),
                  
-                 tabPanel(
-                   title="Gene Connectivity",
-                   "information about Gene Connectivity",
+                 tabPanel(title="Gene Connectivity",
+                   h3("Cluster Genes"),
+                   p('Creates modules which are clusters of genes that are hightly co-expressed'),
+                   h5("Plot Types"),
+                   h6("Heatmap"),  
+                   p("up-regulated or down-regulated network plot"),
+                   h6("Network"),  
+                   p("up regulated or down-regulated network plot where nodes are genes and the weight of the edges corresponds to the co-expression levels between its endpoints"),
+                   h6("Binarized heatmap"),  
+                   p("up-regulated or down-regulated binary co-expression sub-network"),
+                   
+                   br(),
+                   em("Note: This tool is under construction"),
+                   
                  ),
                  
                  
                  tabPanel(
                    title="Functional Outliers",
-                   "information about Functional Outliers",
+                   "Functional outliers are genes that have been identified to be potentially dysregulated. 
+                   They are the genes that are Differentially Expressed but do not show local co-expression",
+                 
+                   h5("Analysis Types"),
+                   h6("Coexpression heatmap"),
+                   "- Up and Down regulated",
+                   h6("Network"),
+                   "- Up and Down regulated",
+                   
+                   br(),
+                   em("Note: This tool is under construction"),
+                   
                  ),
                  
                  tabPanel(
                    title="Gene Set Enrichment Analysis",
                    "information about Enrichment Analysis",
+      
+                   br(),
+                   em("Note: This tool is under construction")
                  ),
                  
                )
@@ -135,6 +181,7 @@ ui <- fluidPage(
                ),
                
              )
+             
              
   ),
 )
