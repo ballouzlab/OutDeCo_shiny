df850c6f483a84e31b50679cdc72644b8b764a7f
INDIA-CHARLIE-ECHO-MAN <43439611+INDIA-CHARLIE-ECHO-MAN@users.noreply.github.com>
2022-06-27 13:50:39 +1000

    finished making SHIN17 feature work. Missing features = making delimtier autodetection and setting a line limit for view file

--- server.R
+++ server.R
@@ -2,52 +2,52 @@ server <- function(input, output, session) {
   output$distPlot <- renderPlot({
     hist(rnorm(input$obs), col = 'darkgray', border = 'white')
   })
-
+  
+  observe({
+    # DEFile from fileInput() function
+    ServerDEFile <- req(input$DEFile)
+    
+    # extensions tool for format validation
+    extDEFile <- tools::file_ext(ServerDEFile$datapath)
+    if(is.null(input$DEFile)){return()
+    }else{
+      if (extDEFile == "txt") {
+        label = paste("Delimiters for", extDEFile, "file")
+        choice <-c(Comma=",", Semicolon=";", Tab="\t", Space=" ")
+      }else if (extDEFile == "tsv") {
+        label = paste("Delimiter: Tab")
+        choice <- (Tab="\t")
+      }else {
+        label = paste("Delimiter: Comma")
+        choice <- (Comma=",")
+      }
+      updateRadioButtons(session, "sepButton", label = label, choices = choice)
+    }
+  })
+  
   # reactive converts the upload file into a reactive expression known as data
-  uploadData <- reactive({
+  data <- reactive({
 
-  # DEFile from fileInput() function
-  ServerDEFile <- input$DEFile
+    # DEFile from fileInput() function
+    ServerDEFile <- input$DEFile
 
-  # extensions tool for format validation
-  extDEFile <- tools::file_ext(ServerDEFile$datapath)
+    # extensions tool for format validation
+    extDEFile <- tools::file_ext(ServerDEFile$datapath)
 
-  # file format checking
-  req(ServerDEFile)
-  validate(need(extDEFile == c("csv", "tsv", "txt"), "Please upload a csv, tsv or txt file."))
+    # file format checking
+    req(ServerDEFile)
+    # validate(need(extDEFile == c("csv", "tsv", "txt"), "Please upload a csv, tsv or txt file."))
 
-  # convert data into file format
-  if(is.null(extDEFile)){return()} 
+    # convert data into file format
+    if(is.null(extDEFile)){return()}
 
-  if (extDEFile == "txt") {
-    choice <-c(Comma=",", Semicolon=";", Tab="\t", Space=" ")
-    updateRadioButtons(session, "sepButton",
-                      label = paste("Delimiter Selection for", extDEFile),
-                      choices = choice
-    )
-  }
-  else if (extDEFile == "csv") {
-    choice <-c(",")
-    updateRadioButtons(session, "sepButton",
-                      label = paste("Delimiter: Comma"),
-                      choices = choice
-    )
-  }
-  else {
-    choice <-c("\t")
-    updateRadioButtons(session, "sepButton",
-                      label = paste("Delimiter: Tab"),
-                      choices = choice
-    )
-  }
-
-  read.table(file=ServerDEFile$datapath, sep=input$sepButton)
+    read.table(file=ServerDEFile$datapath, sep=input$sepButton)
   })
 
   # creates reactive table called DEFileContent
   output$DEFileContent <- renderTable({
-  if(is.null(uploadData())){return ()}
-  uploadData()
+    if(is.null(data())){return ()}
+    data()
   })
 
   # handles rendering of reactive object on tb on ui
