62155bcdf83b4031d54c8df1d0eda72024964df8
vandana <vandanaprathapar@gmail.com>
2022-07-04 11:55:28 +1000

    implemented binarized heatmap

--- src/plot_functions.R
+++ src/plot_functions.R
@@ -14,27 +14,34 @@
 
 plot_coexpression_heatmap <- function(coexp, cluster_output,
                                       col_map = viridis(100),
-                                      filt = FALSE, filt_min = 6) {
-
-
+                                      filt = FALSE, filt_min = 6, 
+                                      flag_plot_bin= TRUE) {
+  
+  
     m <- match( rownames(coexp), cluster_output$clusters[,1] )
     temp_col <- as.character(cluster_output$clusters[m,4])
-
+    
     clust_size <- plyr::count( cluster_output$clusters$labels )
     clust_keep <-  clust_size[clust_size[,2] < filt_min ,1]
     genes_keep <- !is.na(match( cluster_output$clusters$labels[m], clust_keep))
-
+    
     temp_filt_col <- temp_col
     if(filt==TRUE){ temp_filt_col[!genes_keep] <- "white" }
-
-    heatmap.2(coexp, density.info = "none", trace = "none",
+    
+    if(flag_plot_bin == TRUE) { 
+      temp = cluster_output$distance_matrix
+    } else {
+      temp = coexp
+    } 
+    
+    heatmap.2(temp, density.info = "none", trace = "none",
               col = col_map,
               Rowv = cluster_output$dendrogram,
               Colv = cluster_output$dendrogram,
               RowSideColors =  temp_col,
               ColSideColors = temp_filt_col,
-              cexRow = 0.5, cexCol = 0.5, main="" )
-
+              cexRow = 0.5, cexCol = 0.5, main="" )  
+  
 }
 
 
